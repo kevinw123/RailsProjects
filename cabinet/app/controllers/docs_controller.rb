07d3c73b8bb74b7bac386b482a4e783a89b1c522
Kevin Wong <kevinwong_895@hotmail.com>
2016-09-05 00:30:18 -0700

    Add doc views and define actions

--- cabinet/app/controllers/docs_controller.rb
+++ cabinet/app/controllers/docs_controller.rb
@@ -1,9 +1,11 @@
 class DocsController < ApplicationController
-
+	before_action :find_doc, only: [:show, :edit, :update, :destroy]
 	def index
+		@docs = Doc.all.order("created_at DESC")
 	end
 
 	def show
+
 	end
 
 	def new
@@ -11,7 +13,7 @@ class DocsController < ApplicationController
 	end
 
 	def create
-		@doc = Doc.new(doc_parms)
+		@doc = Doc.new(doc_params)
 
 		if @doc.save
 			redirect_to @doc
@@ -32,8 +34,10 @@ class DocsController < ApplicationController
 	private
 
 		def find_doc
+			@doc = Doc.find(params[:id])
 		end
 
 		def doc_params
+			params.require(:doc).permit(:title, :content)
 		end
 end
